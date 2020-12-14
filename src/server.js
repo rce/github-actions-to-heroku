a8eeb79ac0e6d5bdd44bf5f5d75a33170ae62cac
Henry Heikkinen <rce@rce.fi>
2020-12-14 21:42:13 +0200

    Connect to the database :)

--- src/server.js
+++ src/server.js
@@ -1,7 +1,21 @@
 const express = require("express")
+const db = require("knex")({
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+  searchPath: ['public'],
+})
 
 const PORT = Number(process.env.PORT || "3000")
 
 const app = express()
-app.get("/", (req, res) => res.send("Hello, world!"))
+
+app.get("/", async (req, res) => {
+  const { rows } = await db.raw(`
+    SELECT
+      'Hello, world!' as greeting,
+      current_timestamp as time
+  `)
+  res.status(200).json(rows[0])
+})
+
 app.listen(PORT)
